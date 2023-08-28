d6ad5b9aa199fcd5a92fd850d45f465e9302911e
yuheng319 <52266856+yuheng319@users.noreply.github.com>
2023-08-28 19:45:25 +0800

    Modify QA H5

--- frontend/views/qah5/qaone.php
+++ frontend/views/qah5/qaone.php
@@ -54,7 +54,7 @@ $this->title = $qa['topic'];
 
                     <div class="text-center m-t-30">
             <label class="h5-btn-green-big answer-btn"  data-value="<?php echo $qa['st_selected']; ?>
-" data-detail="">
+" data-qa="" data-story="">
                 提交
             </label>
         </div>
@@ -62,6 +62,22 @@ $this->title = $qa['topic'];
        
     </div>
 
+</div>
+<!-- 按钮：用于打开模态框 -->
+<div class="modal fade" id="h5-null" tabindex="-1" style="display: none;" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <span class="close delete-note  m-t-15 m-r-20  fs-24 absolute  z-9999 iconfont iconbtn-guanbi" data-dismiss="modal" style="top: 15px;right: 15px;"></span>
+            <div class="p-20-40 relative h5 m-t-30" name="loginStr" style="width: 600px;">
+                <div>
+                    <div class="fs-36 text-F6 text-center bold">
+                       请选择答案
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <!-- 按钮：用于打开模态框 -->
--- template/html/h5/app.js
+++ template/html/h5/app.js
@@ -20,17 +20,53 @@ $(function () {
     //判断是否答对
      $(".answer-btn").on('click',function () {
         var that=$(this);
+        var qa_id=that.attr("data-qa");
+        var story_id=that.attr("data-story");
         var v_ture=that.attr("data-value");
         var v_detail=that.attr("data-detail");
         var v_select=$("input[name='answer']:checked").val();
         if(v_select==null){
-            alert("什么也没选中!");
+            $("#h5-null").modal('show');
         }
         else if(v_ture==v_select){
           $("#h5-right").modal('show');
         }
         else{
           $("#h5-worry").modal('show');
+        }
+
+        if(v_select!=null){
+          $.ajax({
+                type: "GET", //用POST方式传输
+                dataType: "json", //数据格式:JSON
+                async: false,
+                url: '/qa/add_user_answer',
+                data:{
+                    qa_id=qa_id,
+                    answer=v_select,
+                    story_id=story_id
+                },
+                error: function (XMLHttpRequest, textStatus, errorThrown) {
+                    console.log("ajax请求失败:"+XMLHttpRequest,textStatus,errorThrown);
+                    $.alert("网络异常，请检查网络情况");
+                },
+                success: function (data, status){
+                    var dataContent=data;
+                    var dataCon=$.toJSON(dataContent);
+                    var obj = eval( "(" + dataCon + ")" );//转换后的JSON对象
+                    //console.log("ajax请求成功:"+data.toString())
+                    //新消息获取成功
+                    if(obj["code"]==200){
+                     
+
+                    }
+                    //新消息获取失败
+                    else{
+                        $.alert(obj.msg)
+                    }
+
+                }
+            });
         }
     })
 
