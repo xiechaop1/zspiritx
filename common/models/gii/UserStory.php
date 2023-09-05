2682879bdb421ad0d7d71edcbf16e828d2cf6b56
xiechao <xiechao2@tal.com>
2023-09-05 22:37:04 +0800

    add new models

--- common/models/gii/UserStory.php
+++ common/models/gii/UserStory.php
@@ -37,7 +37,7 @@ class UserStory extends \yii\db\ActiveRecord
     {
         return [
             [['user_id', 'role_id', 'story_id', 'session_id', 'team_id', 'building_id', 'status', 'created_at', 'updated_at'], 'integer'],
-            [['goal', 'goal_right'], 'string'],
+            [['goal', 'goal_correct'], 'string'],
         ];
     }
 
