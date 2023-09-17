a3570aa11e542f8a5421d2bae16fd76978e32c68
xiechao <xiechao2@tal.com>
2023-09-17 23:14:34 +0800

    add new models

---
+++ frontend/controllers/Baggageh5Controller.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liyifei
+ * Date: 2019/4/14
+ * Time: 下午11:30
+ */
+
+namespace frontend\controllers;
+
+
+use liyifei\base\controllers\ViewController;
+use yii\web\Controller;
+
+class Baggageh5Controller extends Controller
+{
+    public $layout = '@frontend/views/layouts/main_w.php';
+
+    public function actions()
+    {
+        $request = \Yii::$app->request;
+
+        return [
+            'all' => [
+                'class' => 'frontend\actions\baggageH5\Baggage',
+            ],
+
+        ];
+    }
+}
