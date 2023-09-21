2aa3b67541ab28e36e00152350685b9e859ada80
yuheng319 <52266856+yuheng319@users.noreply.github.com>
2023-09-22 00:23:04 +0800

    Update map.js

--- template/html/h5/map.js
+++ template/html/h5/map.js
@@ -223,7 +223,7 @@ $(function () {
                         };
                         markers.push(marker)
                     }
-                    removeMarkers();
+                    // removeMarkers();
 
                     drawPoi(markers);
 
