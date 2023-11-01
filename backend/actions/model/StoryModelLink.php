ef6045b165b7d0487f8a2ed432e19f416db8b4c7
xiechao <xiechao2@tal.com>
2023-11-01 18:32:44 +0800

    add new models

--- backend/actions/model/StoryModelLink.php
+++ backend/actions/model/StoryModelLink.php
@@ -98,10 +98,12 @@ class StoryModelLink extends Action
         $storyModelDatas = \common\models\StoryModels::find()->orderBy(['id' => SORT_DESC])->all();
         $storyModelList = ['-1' => '无'];
         foreach ($storyModelDatas as $storyModel) {
-            $storyModelList[$storyModel->id] = !empty($storyModel->story_model_name)
+            $storyModelList[$storyModel->id] = $storyModel->story->title . ' ' .
+                !empty($storyModel->story_model_name)
                 ? $storyModel->story_model_name . ' [' . $storyModel->id . '|' . $storyModel->story_model_detail_id . ']' :
                     !empty($storyModel->model->model_name)
                         ? $storyModel->model->model_name . ' [' . $storyModel->id . '|' . $storyModel->story_model_detail_id . ']' : '[' . $storyModel->id . '|' . $storyModel->story_model_detail_id . ']';
+            $storyModelList[$storyModel->id] .= '(' . $storyModel->model_inst_u_id . ')';
             ;
         }
 
