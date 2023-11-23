dd6281f2a57e1a788df06128097f77c64075cf81
xiechao <xiechao2@tal.com>
2023-11-23 15:03:47 +0800

    add new models

---
+++ frontend/actions/compassh5/Compass.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: leeyifiei
+ * Date: 2019/4/25
+ * Time: 1:51 PM
+ */
+
+namespace frontend\actions\compassh5;
+
+
+use common\definitions\Common;
+use common\models\Story;
+use yii\base\Action;
+use kartik\form\ActiveForm;
+use liyifei\base\helpers\Net;
+use common\models\Qa;
+
+use Yii;
+use yii\helpers\ArrayHelper;
+use yii\web\NotFoundHttpException;
+
+class Compass extends Action
+{
+
+    
+    public function run()
+    {
+        $userId = !empty($_GET['user_id']) ? $_GET['user_id'] : 0;
+        $storyId = !empty($_GET['story_id']) ? $_GET['story_id'] : 0;
+        $sessionId = !empty($_GET['session_id']) ? $_GET['session_id'] : 0;
+        $storyStageId = !empty($_GET['story_stage_id']) ? $_GET['story_stage_id'] : 0;
+        $teamId = !empty($_GET['team_id']) ? $_GET['team_id'] : 0;
+        $userLng = !empty($_GET['user_lng']) ? $_GET['user_lng'] : 0;
+        $userLat = !empty($_GET['user_lat']) ? $_GET['user_lat'] : 0;
+        $disRange = !empty($_GET['dis_range']) ? $_GET['dis_range'] : 0;
+
+        return $this->controller->render('compass', [
+            'userId'        => $userId,
+            'storyId'       => $storyId,
+            'sessionId'     => $sessionId,
+            'storyStageId'  => $storyStageId,
+            'teamId'        => $teamId,
+            'userLng'       => $userLng,
+            'userLat'       => $userLat,
+            'disRange'      => $disRange,
+        ]);
+    }
+}
---
+++ frontend/controllers/Compasseh5Controller.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liyifei
+ * Date: 2019/4/14
+ * Time: 下午11:30
+ */
+
+namespace frontend\controllers;
+
+
+use liyifei\base\controllers\ViewController;
+use yii\web\Controller;
+
+class Compasseh5Controller extends Controller
+{
+    public $layout = '@frontend/views/layouts/main_h5.php';
+
+    public function actions()
+    {
+        $request = \Yii::$app->request;
+
+        return [
+            'all' => [
+                'class' => 'frontend\actions\compassh5\Knowledge',
+            ],
+            'set' => [
+                'class' => 'frontend\actions\compassh5\Set',
+            ],
+
+        ];
+    }
+}
