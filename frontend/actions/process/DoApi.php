c5dec6fd2d39ce21463f3cd3a1db5c5a85c25f21
xiechao <xiechao2@tal.com>
2023-11-30 10:23:16 +0800

    add new models

--- frontend/actions/process/DoApi.php
+++ frontend/actions/process/DoApi.php
@@ -1088,6 +1088,7 @@ class DoApi extends ApiAction
                     $modelUId = $storyModel->model_u_id;
                     $expirationInterval = 3600;
                     Yii::$app->act->add((int)$this->_sessionId, 0, (int)$this->_storyId, (int)$this->_userId, $modelUId, Actions::ACTION_TYPE_MODEL_DISPLAY, $expirationInterval);
+                    $minCt = 0;
                     break;
                 default:
                     break;
