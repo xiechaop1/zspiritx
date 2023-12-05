e031f2ae359089085fabca7fdc01c872e87bcffd
xiechao <xiechao2@tal.com>
2023-12-05 10:51:32 +0800

    add new models

--- frontend/assets/Qah5Asset.php
+++ frontend/assets/Qah5Asset.php
@@ -47,7 +47,7 @@ class Qah5Asset extends AssetBundle
         self::HOST . 'js/owl.carousel/owl.carousel.js',
         self::HOST . 'js/getOptions.js',
         self::HOST . 'js/jquery.SuperSlide.2.1.3.js',
-        self::HOST . 'html/h5/app.js',
+        'html/h5/app.js',
         ];
 
 
