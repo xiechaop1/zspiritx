5180e8bd1e1ffc5e8da59906d5442adf919dfba9
xiechao <xiechao2@tal.com>
2023-12-12 17:45:35 +0800

    add new models

--- template/html/h5/app.js
+++ template/html/h5/app.js
@@ -550,7 +550,7 @@ $(function () {
         var knowledge_desc = obj.find("input[NAME='knowledge_content']").val();
 
         $('#knowledge_title').html(knowledge_title);
-        $('#knowledge_image').html(knowledge_image);
+        $('#knowledge_image').html('<img src=' + knowledge_image + ' style="width: 100%; height: auto;">');
         $('#knowledge_desc').html(knowledge_desc);
 
         $('#knowledge_detail').modal('show');
