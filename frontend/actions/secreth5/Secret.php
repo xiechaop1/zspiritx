86c0035f3db9726280995295f3853829ffab7b7b
xiechao <xiechao2@tal.com>
2023-12-15 01:01:57 +0800

    add new models

--- frontend/actions/secreth5/Secret.php
+++ frontend/actions/secreth5/Secret.php
@@ -28,16 +28,40 @@ class Secret extends Action
     {
         $userId = !empty($_GET['user_id']) ? $_GET['user_id'] : 0;
         $sessionId = !empty($_GET['session_id']) ? $_GET['session_id'] : 0;
+        $sessionStageId = !empty($_GET['session_stage_id']) ? $_GET['session_stage_id'] : 0;
         $storyId = !empty($_GET['story_id']) ? $_GET['story_id'] : 0;
 
         $pinCode = !empty($_GET['pin_code']) ? $_GET['pin_code'] : 0;
+
+        $qaId = !empty($_GET['qa_id']) ? $_GET['qa_id'] : 0;
+
+        if (!empty($qaId)) {
+            $qaOne = Qa::find()
+                ->where([
+                    'id'    => $qaId,
+                ])
+                ->one();
+
+            if (empty($qaOne)) {
+                throw new NotFoundHttpException('QA not found');
+            }
+            $qaOne = $qaOne->toArray();
+            $qaOne['selected_json'] = \common\helpers\Common::isJson($qaOne['selected']) ? json_decode($qaOne['selected'], true) : $qaOne['selected'];
+            $qaOne['attachment'] = \common\helpers\Attachment::completeUrl($qaOne['attachment'], true);
+
+        }
 
         return $this->controller->render('secret', [
             'params'        => $_GET,
             'userId'        => $userId,
             'sessionId'     => $sessionId,
+            'sessionStageId'=> $sessionStageId,
+            'qaId'          => $qaId,
             'storyId'       => $storyId,
             'pinCode'       => $pinCode,
+            'rightAnswer'   => $qaOne['st_answer'],
+            'stSelected'    => $qaOne['st_selected'],
+            'qa'         => $qaOne,
         ]);
     }
 }
--- frontend/views/secreth5/secret.php
+++ frontend/views/secreth5/secret.php
@@ -28,7 +28,26 @@ $this->registerMetaTag([
 $this->title = '密码锁';
 
 ?>
-<input type="hidden" name="pin_code" value="<?= $pinCode ?>">
+<style>
+    .hide {display: none;}
+</style>
+<input type="hidden" name="pin_code" value="<?= $stSelected ?>">
+<input type="hidden" name="user_id" value="<?= $userId ?>">
+<input type="hidden" name="session_id" value="<?= $sessionId ?>">
+<input type="hidden" name="session_stage_id" value="<?= $sessionStageId ?>">
+<input type="hidden" name="qa_id" id="qa_id" value="<?= $qaId ?>">
+<input type="hidden" name="story_id" id="story_id" value="<?= $storyId ?>">
+
+
+<div class="w-100 m-auto">
+    <audio controls id="audio_right" class="hide">
+        <source src="../../static/audio/qa_right.mp3" type="audio/mpeg">
+        您的浏览器不支持 audio 元素。
+    </audio>
+    <audio controls id="audio_wrong" class="hide">
+        <source src="../../static/audio/qa_wrong.mp3" type="audio/mpeg">
+        您的浏览器不支持 audio 元素。
+    </audio>
 
 <div class="btn-m-green m-t-30 float-right m-r-20" id="return_btn">
   返回
@@ -38,12 +57,15 @@ $this->title = '密码锁';
 
 <div class="lock">
   <div class="screen">
-    <div class="code">0000</div>
+    <div class="code"><?= str_repeat('0', strlen($stSelected)) ?></div>
     <div class="status">LOCKED</div>
     <div class="scanlines"></div>
   </div>
   <div class="rows">
-    <div class="row">
+    <?php
+    for ($i=0; $i<strlen($stSelected); $i++) {
+      ?>
+      <div class="row">
       <div class="cell">
         <div class="text">0</div>
       </div>
@@ -75,165 +97,196 @@ $this->title = '密码锁';
         <div class="text">9</div>
       </div>
     </div>
-    <div class="row">
-      <div class="cell">
-        <div class="text">0</div>
-      </div>
-      <div class="cell">
-        <div class="text">1</div>
-      </div>
-      <div class="cell">
-        <div class="text">2</div>
-      </div>
-      <div class="cell">
-        <div class="text">3</div>
-      </div>
-      <div class="cell">
-        <div class="text">4</div>
-      </div>
-      <div class="cell">
-        <div class="text">5</div>
-      </div>
-      <div class="cell">
-        <div class="text">6</div>
-      </div>
-      <div class="cell">
-        <div class="text">7</div>
-      </div>
-      <div class="cell">
-        <div class="text">8</div>
-      </div>
-      <div class="cell">
-        <div class="text">9</div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="cell">
-        <div class="text">0</div>
-      </div>
-      <div class="cell">
-        <div class="text">1</div>
-      </div>
-      <div class="cell">
-        <div class="text">2</div>
-      </div>
-      <div class="cell">
-        <div class="text">3</div>
-      </div>
-      <div class="cell">
-        <div class="text">4</div>
-      </div>
-      <div class="cell">
-        <div class="text">5</div>
-      </div>
-      <div class="cell">
-        <div class="text">6</div>
-      </div>
-      <div class="cell">
-        <div class="text">7</div>
-      </div>
-      <div class="cell">
-        <div class="text">8</div>
-      </div>
-      <div class="cell">
-        <div class="text">9</div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="cell">
-        <div class="text">0</div>
-      </div>
-      <div class="cell">
-        <div class="text">1</div>
-      </div>
-      <div class="cell">
-        <div class="text">2</div>
-      </div>
-      <div class="cell">
-        <div class="text">3</div>
-      </div>
-      <div class="cell">
-        <div class="text">4</div>
-      </div>
-      <div class="cell">
-        <div class="text">5</div>
-      </div>
-      <div class="cell">
-        <div class="text">6</div>
-      </div>
-      <div class="cell">
-        <div class="text">7</div>
-      </div>
-      <div class="cell">
-        <div class="text">8</div>
-      </div>
-      <div class="cell">
-        <div class="text">9</div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="cell">
-        <div class="text">0</div>
-      </div>
-      <div class="cell">
-        <div class="text">1</div>
-      </div>
-      <div class="cell">
-        <div class="text">2</div>
-      </div>
-      <div class="cell">
-        <div class="text">3</div>
-      </div>
-      <div class="cell">
-        <div class="text">4</div>
-      </div>
-      <div class="cell">
-        <div class="text">5</div>
-      </div>
-      <div class="cell">
-        <div class="text">6</div>
-      </div>
-      <div class="cell">
-        <div class="text">7</div>
-      </div>
-      <div class="cell">
-        <div class="text">8</div>
-      </div>
-      <div class="cell">
-        <div class="text">9</div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="cell">
-        <div class="text">0</div>
-      </div>
-      <div class="cell">
-        <div class="text">1</div>
-      </div>
-      <div class="cell">
-        <div class="text">2</div>
-      </div>
-      <div class="cell">
-        <div class="text">3</div>
-      </div>
-      <div class="cell">
-        <div class="text">4</div>
-      </div>
-      <div class="cell">
-        <div class="text">5</div>
-      </div>
-      <div class="cell">
-        <div class="text">6</div>
-      </div>
-      <div class="cell">
-        <div class="text">7</div>
-      </div>
-      <div class="cell">
-        <div class="text">8</div>
-      </div>
-      <div class="cell">
-        <div class="text">9</div>
-      </div>
-    </div>
+    <?php
+    }
+    ?>
+<!--    <div class="row">-->
+<!--      <div class="cell">-->
+<!--        <div class="text">0</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">1</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">2</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">3</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">4</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">5</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">6</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">7</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">8</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">9</div>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--    <div class="row">-->
+<!--      <div class="cell">-->
+<!--        <div class="text">0</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">1</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">2</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">3</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">4</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">5</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">6</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">7</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">8</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">9</div>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--    <div class="row">-->
+<!--      <div class="cell">-->
+<!--        <div class="text">0</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">1</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">2</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">3</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">4</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">5</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">6</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">7</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">8</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">9</div>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--    <div class="row">-->
+<!--      <div class="cell">-->
+<!--        <div class="text">0</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">1</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">2</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">3</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">4</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">5</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">6</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">7</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">8</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">9</div>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--    <div class="row">-->
+<!--      <div class="cell">-->
+<!--        <div class="text">0</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">1</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">2</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">3</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">4</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">5</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">6</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">7</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">8</div>-->
+<!--      </div>-->
+<!--      <div class="cell">-->
+<!--        <div class="text">9</div>-->
+<!--      </div>-->
+<!--    </div>-->
   </div>
 </div>
+
+    <div class="hide answer-right" id="answer-right-box" style="position: absolute; background-color: #1F2628 !important; z-index: 9999999999; width: 90%;font-size: 14px; top: 30px; left: 15px;">
+        <div class="m-t-30 col-sm-12 col-md-12 p-40">
+            <img src="../../static/img/qa/Frame@2x.png" style="width: 100%; height: auto;" alt="" class="img-responsive  d-block m-auto"/>
+            <?php
+            if (!empty($qa['score'])) {
+                ?>
+                <div style="clear:both; text-align: center;">
+                        <span>
+                    <img src="../../static/img/qa/gold.png" alt="" style="width: 75px; height: 75px; vertical-align: middle;" class=""/>
+                            </span>
+
+                    <span class="answer-detail" style="box-sizing: border-box; color: yellow; font-size: 24px; font-weight: 500; line-height: 24px;letter-spacing: 2px; text-align: center;">
+                    +<?= $qa['score'] ?>枚
+                        </span>
+                </div>
+                <?php
+            }
+            ?>
+            <div class="answer-title m-t-40" style="color: yellow; font-size: 36px; font-weight: 500; line-height: 28px;letter-spacing: 2px;  text-align: center;">
+                <?= $qa['st_selected'] ?>
+            </div>
+            <div class="answer-detail m-t-40" style="color: yellow; line-height: 24px; font-size: 14px; margin-top: 14px; margin: 15px;">
+                <?= $qa['st_answer'] ?>
+            </div>
+        </div>
+
+    </div>
