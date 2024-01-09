150d793b32ac06f9870e082402f6fcd8d8fdeaf2
xiechao <xiechao2@tal.com>
2024-01-09 10:20:56 +0800

    add new models

---
+++ frontend/views/site/error.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: leeyifiei
+ * Date: 2019/3/4
+ * Time: 3:14 PM
+ */
+
+/**
+ * @var \yii\web\View $this ;
+ */
+
+/**
+ * @var \common\models\QA $qa
+ */
+
+\frontend\assets\Qah5Asset::register($this);
+
+$this->registerMetaTag([
+    'name' => 'referrer',
+    'content' => 'no-referrer',
+]);
+//$this->registerMetaTag([
+//    'name' => 'viewport',
+//    'content' => 'width=device-width; initial-scale=1.0',
+//]);
+
+$this->title = '消息';
+
+?>
+<div class="w-100 m-auto">
+
+    <div class="p-20 bg-black">
+        <div class="w-100 p-30  m-b-10">
+            <div class="w-1-0 d-flex">
+                <div class="fs-30 bold w-100 text-FF title-box-border">
+                    <div class="npc-name">
+                        消息
+                    </div>
+                    服务出现了异常，请您重试<a href="javascript:location.reload();">刷新</a>页面
+                </div>
+            </div>
+            <div class="btn-m-green m-t-30 float-right m-r-20" id="msg_return_btn">
+                返回
+            </div>
+        </div>
+        </div>
+
+    </div>
+
+</div>
