fcf8d31f46bfb143cd6323dbee35fbca80565760
xiechao <xiechao2@tal.com>
2024-01-23 23:38:33 +0800

    fig bug

--- backend/views/model/story_model_edit.php
+++ backend/views/model/story_model_edit.php
@@ -135,6 +135,7 @@ echo \dmstr\widgets\Alert::widget();
 //                'directory' => 'cover/' . Date('Y/m/')
             ])->label('图片/影音文件');
             echo $form->field($storyModel, 'use_group_name')->textInput(['value' => $storyModel->use_group_name])->label('背包分组');
+            echo $form->field($storyModel, 'sort_by')->textInput(['value' => $storyModel->sort_by])->label('出现顺序');
             echo $form->field($storyModel, 'rate')->textInput(['value' => $storyModel->rate])->label('出现概率');
             echo $form->field($storyModel, 'timebegin')->textInput(['value' => $storyModel->timebegin])->label('开始时间');
             echo $form->field($storyModel, 'timeend')->textInput(['value' => $storyModel->timeend])->label('结束时间');
