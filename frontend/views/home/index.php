cea5afcd7669fffc936861e376662fac3e46b336
xiechao <xiechao2@tal.com>
2024-03-04 16:14:08 +0800

    fix bug

--- frontend/views/home/index.php
+++ frontend/views/home/index.php
@@ -82,7 +82,7 @@ $this->title = 'AR剧本杀';
       <!-- onclick="Unity.call('WebViewOff&StartARScene');" -->
 
       <div class="btn-m-green m-t-30 float-right m-r-20" style="position: absolute; right: 0px; margin: 35px;">
-        <a href="/home/my">我的</a>
+        <a href="/home/my?unity_version=<?= $unityVersion ?>">我的</a>
       </div>
 
       <img decoding="async"  src="<?= $image ?>" alt="First slide" class="img-w-100">
@@ -128,7 +128,7 @@ $this->title = 'AR剧本杀';
     <input type="hidden" name="storyId" value="<?= $story->id ?>">
     <input type="hidden" name="orderStatus" value="<?= !empty($ordersMap[$story->id]) ? $ordersMap[$story->id] : 0 ?>">
     <div class="btn-m-green m-t-30 float-right m-r-20" style="position: absolute; right: 0px; margin: 35px;">
-      <a href="/home/my">我的</a>
+      <a href="/home/my?unity_version=<?= $unityVersion ?>">我的</a>
     </div>
     <img decoding="async"  src="<?= \common\helpers\Attachment::completeUrl($story->cover_image) ?>" alt="First slide" class="img-w-100">
     <div class="text-content">
