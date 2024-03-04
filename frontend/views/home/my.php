be2543698e5404174a153fde5df62210f40f4ea3
xiechao <xiechao2@tal.com>
2024-03-04 16:17:13 +0800

    fix bug

--- frontend/views/home/my.php
+++ frontend/views/home/my.php
@@ -95,7 +95,7 @@ $this->title = '灵镜新世界-我的';
                         <!-- <input class="form-check-input" type="radio" name="knowledge" value="' . $item->id . '" id="legal_person_yes_' . $item->id . '" > -->
                         <label class="form-check-label fs-30 answer-btn">
                             <!--                  <span class="answer-tag"></span>-->
-                            <span ><a href="/home/index">返回</a></span>
+                            <span ><a href="/home/index?unity_version=<?= $unityVersion ?>">返回</a></span>
                         </label>
                     </div>
                 </div>
