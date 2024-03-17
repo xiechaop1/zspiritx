aa420a5408387f7bd6e714a6c4702693eeaa6783
xiechao <xiechao2@tal.com>
2024-03-17 18:59:58 +0800

    fix bug

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -10,19 +10,21 @@ use yii\web\View;
  */
 class AppAsset extends AssetBundle
 {
+    const HOST_URL = 'https://zspiritx.oss-cn-beijing.aliyuncs.com/backend_resource/';
+
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
-        'css/theme.css',
-        'css/bootstrap-datepicker.css',
-        'css/AdminLTE.min1.0.css'
+        self::HOST_URL . 'css/site.css',
+        self::HOST_URL . 'css/theme.css',
+        self::HOST_URL . 'css/bootstrap-datepicker.css',
+        self::HOST_URL . 'css/AdminLTE.min1.0.css'
     ];
     public $js = [
-        'script/common.js',
-        'script/form-check.js',
-        'script/bootstrap-datepicker.js',
-        'script/app.js'
+        self::HOST_URL . 'script/common.js',
+        self::HOST_URL . 'script/form-check.js',
+        self::HOST_URL . 'script/bootstrap-datepicker.js',
+        self::HOST_URL . 'script/app.js'
     ];
     
     public $jsOptions = [
--- t.php
+++ t.php
@@ -1,21 +1,92 @@
 <?php
 
-$r = [
-		[['avatar', 'uname', 'mobile', 'email',
-				'birthday', 'work_start_date', 'current_company',
-				'current_post', 'foreign_language', 'university', 'job_status', 'expect_post',
-				'document_file', 'expect_salary', 'resource', 'expect_career',
-				'skills', 'certification', 'self_evaluation', 'interview',
-				'education_experience', 'work_experience', 'project_experience',
-				'education', 'national', 'language_skills', 'training',
-				'nation_group', 'birthplace', 'census', 'marry', 'political', 'gender',
-				'fertility', 'work_years', 'user_no', 'city_str', 'expect_city_str'], 'string'],
-		[['current_salary', 'other_income'], 'string'],
-		[['city_id', 'age',  'expect_industry', 'current_industry', 'document_status', 'created_at', 'updated_at', 'status'], 'integer'],
-];
+//$r = [
+//		[['avatar', 'uname', 'mobile', 'email',
+//				'birthday', 'work_start_date', 'current_company',
+//				'current_post', 'foreign_language', 'university', 'job_status', 'expect_post',
+//				'document_file', 'expect_salary', 'resource', 'expect_career',
+//				'skills', 'certification', 'self_evaluation', 'interview',
+//				'education_experience', 'work_experience', 'project_experience',
+//				'education', 'national', 'language_skills', 'training',
+//				'nation_group', 'birthplace', 'census', 'marry', 'political', 'gender',
+//				'fertility', 'work_years', 'user_no', 'city_str', 'expect_city_str'], 'string'],
+//		[['current_salary', 'other_income'], 'string'],
+//		[['city_id', 'age',  'expect_industry', 'current_industry', 'document_status', 'created_at', 'updated_at', 'status'], 'integer'],
+//];
+//
+//foreach ($r as $one) {
+//	foreach ($one[0] as $col) {
+//		echo 'public $' . $col . ";\n";
+//	}
+//}
 
-foreach ($r as $one) {
-	foreach ($one[0] as $col) {
-		echo 'public $' . $col . ";\n";
-	}
+
+$list = [1,2,3];
+$num = 3;
+
+function f($list, $num, $arrA, $arrB, $flag, $ret1, $ret2) {
+    if ($flag == 1) {
+        // 如果flag是1的话就从开始的位置取数字
+
+        // 如果A和B的数组长度相等，就把数字给A，否则给B
+        if (sizeof($arrA) == sizeof($arrB)) {
+            $arrA[] = array_shift($list);
+        } else {
+            $arrB[] = array_shift($list);
+        }
+    } else {
+        // 否则就从尾部的位置取数字
+
+        // 如果A和B的数组长度相等，就把数字给A，否则给B
+        if (sizeof($arrA) == sizeof($arrB)) {
+            $arrA[] = array_pop($list);
+        } else {
+            $arrB[] = array_pop($list);
+        }
+    }
+
+    // 如果剩余的牌只有1张了，就一定给某一个人，当前函数就结束了
+    if (sizeof($list) == 1) {
+        if (sizeof($arrA) == sizeof($arrB)) {
+            $arrA[] = array_shift($list);
+        } else {
+            $arrB[] = array_shift($list);
+        }
+
+        // 求和，如果A的大于B的，就记录A赢1次，一共1次。否则就记录A赢0次，一共1次（数组返回）
+        if (sum($arrA) > sum($arrB)) {
+            return [1, 1];
+        } else {
+            return [0, 1];
+        }
+    }
+    // 递归调用记录结果
+    // 递归从头取
+    $retTmp1 = f($list, $num, $arrA, $arrB, 1, $ret1, $ret2);
+    $ret1 += $retTmp1[0];
+    $ret2 += $retTmp1[1];
+    // 递归从尾取
+    $retTmp2 = f($list, $num, $arrA, $arrB, 0, $ret1, $ret2);
+    $ret1 += $retTmp2[0];
+    $ret2 += $retTmp2[1];
+
+    return [
+        $ret1, $ret2
+    ];
 }
+
+// 数组求和函数
+function sum($arr) {
+    $r = 0;
+    foreach ($arr as $one) {
+        $r += $one;
+    }
+    return $r;
+}
+
+// 调用第一次（头部）
+$result = f($list, $num, [], [], 1, 0, 0);
+// 调用第二次（尾部）
+$result = f($list, $num, [], [], 0, $result[0], $result[1]);
+
+var_dump($result);
