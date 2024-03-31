7f74da2bd3ae7a4929c0b0039772a201f411b30e
yuheng319 <52266856+yuheng319@users.noreply.github.com>
2024-03-31 15:25:35 +0800

    Map modify

--- frontend/views/layouts/main_map.php
+++ frontend/views/layouts/main_map.php
@@ -16,7 +16,7 @@ use frontend\assets\AppAsset;
     <meta charset="<?= Yii::$app->charset ?>">
     <!--<meta name="viewport" content="width=750,user-scalable=no, target-densitydpi=device-dpi">
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no, width=device-width">-->
-    <meta name="viewport" content="width=375, user-scalable=no, width=device-width">
+    <meta name="viewport" content="width=375, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="keywords" content=",AR游戏|AR剧本杀|剧本杀|AR教育|儿童剧本杀|素质教育|实景教育|实景剧本杀|实景游戏">
     <meta name="description" content="灵境剧本杀是做实景剧本和玩家之间的关联平台，包括教育类剧本，游戏类剧本，儿童剧本，成人剧本等，可以满足不同年龄段玩家的需求，并且可以亲身体验实景剧本带来的全新玩法">
@@ -34,7 +34,7 @@ use frontend\assets\AppAsset;
     }
      #container {
           height: 100%;
-          width: 100%;
+          width: 375px;
         }
 </style>
 </head>
