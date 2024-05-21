948844fe562dae2949c1db62996cd780fd43a36a
xiechao <xiechao2@tal.com>
2024-05-21 10:12:13 +0800

    fix bug

--- frontend/actions/process/DoApi.php
+++ frontend/actions/process/DoApi.php
@@ -626,7 +626,7 @@ class DoApi extends ApiAction
 
         // Todo: 临时放到这里
         // Todo: 根据位置放置模型（位置从高德取）
-        if ($storyModel->id == 5) {
+        if ($this->_storyId == 5) {
             $userLng = !empty($this->_get['lng']) ? $this->_get['lng'] : 0;
             $userLat = !empty($this->_get['lat']) ? $this->_get['lat'] : 0;
             $setResult = Yii::$app->userModels->setUserModelToLoc($this->_storyId, 0, $userLng, $userLat, 1000, 30);
