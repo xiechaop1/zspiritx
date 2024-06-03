4fe2298b7728a5265ebc762cbf6afb00edfd679e
yuheng319 <52266856+yuheng319@users.noreply.github.com>
2024-06-03 23:54:49 +0800

    Update map.js

--- template/html/h5/map.js
+++ template/html/h5/map.js
@@ -490,7 +490,7 @@ $(function () {
                     // // 线样式还支持 'dashed'
                     // fillColor: marker.fillColor,
                     // zIndex: 10
-                    radius: 50, //半径
+                    radius: 10, //半径
                     borderWeight: 3,
                     strokeColor: "#FF33FF",
                     strokeWeight: 1,
