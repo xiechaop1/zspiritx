d7e9693734de4843bedbfefac8e3b8605fc1d938
xiechao <xiechao2@tal.com>
2024-08-01 22:12:42 +0800

    fix bug

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -78,8 +78,8 @@ $this->title = '庄生科技';
                                             在灵镜新世界中，每个商场、博物馆、公园，甚至是街道，机场等等每个场景，都可能有着不同的故事，等待着你的探索。<br>
                                             在此你还可以结交朋友，组队配合，一起打怪，一起探索，一起解谜，还原故事，感受每个故事背后的道理。<br>
                                             在游玩中学习知识，锻炼技能<br>
-                                            目前在<span class="color-red">丰台凯德茂大峡谷的《侏罗纪之时间裂痕》、自然博物馆的《奇幻之旅》项目已经上线</span>，目前支持<span color="color-red">iOS、华为等支持AR机型</span>，欢迎体验<br>
-                                            <span class="color-red">陶然亭文化之旅、汽车博物馆极速之旅、龙潭湖公园的灵石之谜、颐和园清宫探秘</span>等项目正在建设中，敬请期待！
+                                            目前在<span class="color-red">汽车博物馆极速狂飙、国家植物园桃花蜜语、自然博物馆的《奇幻之旅》项目已经上线</span>，目前支持<span color="color-red">iOS、华为，以及部分支持AR的Android机型都已经支持</span>，欢迎体验<br>
+                                            <span class="color-red">陶然亭文化之旅、龙潭湖公园的灵石之谜、颐和园清宫探秘</span>等项目正在建设中，敬请期待！
 
                                         </p>
                                     </div>
@@ -97,7 +97,7 @@ $this->title = '庄生科技';
                                             </div>
                                             <div class="col-md-6 col-xs-6 m-t-20">
                                                 <p class="text-left">
-                                                    华为下载（支持Mate20以上机型）
+                                                    Android下载
                                                 </p>
                                                 <div class="qr-box-1">
                                                     <img src="<?= $qrCode['huawei']; ?>" class="img-responsive"/>
@@ -145,7 +145,7 @@ $this->title = '庄生科技';
                 <!--Footer Info -->
                 <div class="row footer-info text-center">
                     <div class="col-md-12 col-sm-12 col-xs-12">
-                        <span class="margin-10 footer-m-span white">Copyright © 2023-2023 庄生科技 zspiritx.com.cn 版权所有</span>
+                        <span class="margin-10 footer-m-span white">Copyright © 2023-2024 庄生科技 zspiritx.com.cn 版权所有</span>
                         <span class="margin-10 footer-m-span"><a href="https://beian.miit.gov.cn" class="white">京ICP备2023021255号</a></span>
                     </div>
                 </div>
