f4a0bca1b6f1b11f557bc0e86f20e8ef1b620512
xiechao <xiechao2@tal.com>
2024-08-26 22:15:20 +0800

    fix bug

--- frontend/actions/match/MatchApi.php
+++ frontend/actions/match/MatchApi.php
@@ -721,7 +721,7 @@ class MatchApi extends ApiAction
                 }
 
                     $tsl = $setPlayerAttSpeed - $oldPlayerAttSpeed;
-                    $tsl = $tsl/14*1000;
+                    $tsl = $tsl/8*1000;
                     $scenario[] = [
                         'timeSinceLast' => $tsl,
                         'lstPerforms' => $prePlayerScenario,
