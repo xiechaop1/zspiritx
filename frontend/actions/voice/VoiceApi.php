ce074b3af3d7825e778db5852f4a737cd7e00088
xiechao <xiechao2@tal.com>
2024-09-23 23:44:27 +0800

    fix bug

---
+++ frontend/actions/voice/VoiceApi.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiechao
+ * Date: 2019/11/01
+ * Time: 4:57 PM
+ */
+
+namespace frontend\actions\voice;
+
+
+use common\definitions\Common;
+use common\definitions\ErrorCode;
+use common\extensions\Uploader;
+use common\models\Actions;
+use common\models\ItemKnowledge;
+use common\models\LotteryPrize;
+use common\models\Qa;
+use common\models\SessionQa;
+use common\models\StoryStages;
+use common\models\UserKnowledge;
+use common\models\UserLottery;
+use common\models\UserModels;
+use common\models\UserPrize;
+use common\models\UserQa;
+use common\models\User;
+//use liyifei\base\actions\ApiAction;
+use common\models\UserList;
+use common\models\UserScore;
+use common\models\UserStory;
+use frontend\actions\ApiAction;
+use yii;
+
+class VoiceApi extends ApiAction
+{
+    public $action;
+
+//    public $userId;
+
+    private $_storyId;
+
+    private $_story;
+
+    private $_get;
+
+    public $dirPrefix;
+
+    public function run()
+    {
+        $this->_get = Yii::$app->request->get();
+
+
+        try {
+            $this->_storyId = !empty($this->_get['story_id']) ? $this->_get['story_id'] : 0;
+
+            switch ($this->action) {
+                case 'input':
+                    $ret = $this->input();
+                    break;
+                default:
+                    $ret = [];
+                    break;
+
+            }
+        } catch (\Exception $e) {
+            return $this->fail($e->getMessage());
+        }
+
+        return $this->success($ret);
+    }
+
+    public function input()
+    {
+        return $this->_upload($this->dirPrefix);
+    }
+
+    public function _upload($dirPrefix){
+
+        $config['accessKeyId'] = Yii::$app->params['oss.accesskeyid'];
+        $config['accessKeySecret'] = Yii::$app->params['oss.accesskeysecret'];
+        $config['endpoint'] = Yii::$app->params['oss.endpoint'];
+        $config['bucket'] = Yii::$app->params['oss.bucket'];
+
+        $up = new Uploader('file', $config);
+
+        var_dump($up->getFileInfo());
+        exit;
+        var_dump($_REQUEST);
+
+//        file_put_contents('/tmp/uploadreq.log', var_export($_REQUEST, true), FILE_APPEND);
+//
+//        if (Yii::$app->request->isPost) {
+//            var_dump($_REQUEST);
+//            $needDate = true;
+//            $fileArray = Yii::$app->upload->upload($dirPrefix);
+//            if (empty($fileArray['file_model']) || $fileArray['file_model']->hasError == 0 ) {
+//                $fileTruePath = $fileArray['file_path'];
+////                $uploadShowDir = 'http://file.hewa.cn/';
+////                $uploadShowDir = Yii::$app->upload->uploadShowDir . $dirPrefix . '/';
+//                $subFilePath = $dirPrefix . '/';
+//                if ($needDate) {
+////                    $uploadShowDir .= date('Ymd') . '/';
+//                    $subFilePath .= date('Ymd') . '/';
+//                }
+//                $uploadShowDir = Yii::$app->upload->uploadShowDir . $subFilePath;
+//                $fileName = Yii::getAlias($uploadShowDir . $fileArray['file_name']);
+//
+//                $fileArray['file_path'] = $fileName;
+//
+//                $data = [
+//                    'file_array' => $fileArray,
+//                    'file_name' => $fileName,
+//                    'sub_file_name' => $subFilePath . $fileArray['file_name'],
+//                    'file_true_path' => $fileTruePath,
+//                ];
+//
+//                return $this->success($data);
+//            } else {
+//                return $this->fail(Yii::t('web', 'upload failed'));
+//            }
+//        } else {
+//            return $this->controller->render('upload', [
+//            ]);
+//        }
+
+    }
+
+
+
+
+}
---
+++ frontend/controllers/VoiceController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liyifei
+ * Date: 2019/4/14
+ * Time: 下午11:30
+ */
+
+namespace frontend\controllers;
+
+
+use liyifei\base\controllers\ViewController;
+use yii\web\Controller;
+
+class VoiceController extends Controller
+{
+    public $layout = '@frontend/views/layouts/main_w.php';
+
+    public function actions()
+    {
+        $request = \Yii::$app->request;
+
+        return [
+            'input' => [
+                'class' => 'frontend\actions\voice\VoiceApi',
+                'action' => 'input',
+            ],
+        ];
+    }
+}
