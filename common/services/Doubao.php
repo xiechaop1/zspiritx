291e5f0df893b50b4a2d70b7ad3787da4aff5f05
xiechao <xiechao2@tal.com>
2024-11-06 10:38:52 +0800

    fix bug

--- common/services/Doubao.php
+++ common/services/Doubao.php
@@ -131,7 +131,8 @@ class Doubao extends Component
             'stream' => true,
             'callback' => ['\common\helpers\Stream', 'streamCallbackToDialogAction'],
             'callback_params' => $params,
-            'model' => 'Qwen/Qwen2.5-Image-72B-Instruct',
+//            'model' => 'Qwen/Qwen2.5-Image-72B-Instruct',
+            'model' => 'OpenGVLab/InternVL2-26B',
         ];
 
         $ret = $this->chatWithDoubao($msg, $oldMessages, $extMessages, [$roleTxt], false, $modelParams, true);
