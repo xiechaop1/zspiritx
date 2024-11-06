f64e8557d0ff0a7518f1f4093cf86175d21fe3a5
xiechao <xiechao2@tal.com>
2024-11-07 00:24:21 +0800

    fix bug

--- common/services/Doubao.php
+++ common/services/Doubao.php
@@ -128,9 +128,9 @@ class Doubao extends Component
         ];
 
         $modelParams = [
-            'stream' => true,
-            'callback' => ['\common\helpers\Stream', 'streamCallbackToDialogAction'],
-            'callback_params' => $params,
+//            'stream' => true,
+//            'callback' => ['\common\helpers\Stream', 'streamCallbackToDialogAction'],
+//            'callback_params' => $params,
 //            'model' => 'Qwen/Qwen2.5-Image-72B-Instruct',
             'model' => 'OpenGVLab/InternVL2-26B',
         ];
