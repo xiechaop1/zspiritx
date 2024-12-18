615657241568eefb0fb580e62fd5184580c53a94
xiechao <xiechao2@tal.com>
2024-12-18 17:27:43 +0800

    fix bug

--- template/html/h5/app.js
+++ template/html/h5/app.js
@@ -1688,7 +1688,8 @@ $(function () {
                                 'WebViewOff': 1,
                                 'DebugInfo': isDebug,
                                 'UserId': userId,
-                                'StoryId': storyId
+                                'StoryId': storyId,
+                                'gotoStory': storyId,
                             }
                             var data = $.toJSON(params);
                             console.log(data);
