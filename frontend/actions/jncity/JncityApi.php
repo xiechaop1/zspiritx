391c92b38e2e8410c3e05913c3a1c660386c4353
xiechao <xiechao2@tal.com>
2025-07-30 15:56:13 +0800

    fix bug

--- frontend/actions/jncity/JncityApi.php
+++ frontend/actions/jncity/JncityApi.php
@@ -255,7 +255,7 @@ class JncityApi extends ApiAction
 //                $videoId = $ret['id'];
 //            }
         } catch (\Exception $e) {
-            throw \Exception('生成视频失败: ' . $e->getMessage(), ErrorCode::EBOOK_GEN_VIDEO_FAILED);
+            throw new \Exception('生成视频失败: ' . $e->getMessage(), ErrorCode::EBOOK_GEN_VIDEO_FAILED);
         }
 
         return [
@@ -277,7 +277,7 @@ class JncityApi extends ApiAction
                 $videoId = $ret['id'];
             }
         } catch (\Exception $e) {
-            throw \Exception('生成视频失败: ' . $e->getMessage(), ErrorCode::EBOOK_GEN_VIDEO_FAILED);
+            throw new \Exception('生成视频失败: ' . $e->getMessage(), ErrorCode::EBOOK_GEN_VIDEO_FAILED);
         }
 
         return [
