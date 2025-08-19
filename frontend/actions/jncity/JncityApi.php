4306834ef08bd3c0e325611e271cf181811a7029
xiechao <xiechao2@tal.com>
2025-08-19 17:58:26 +0800

    fix bug

--- frontend/actions/jncity/JncityApi.php
+++ frontend/actions/jncity/JncityApi.php
@@ -311,7 +311,7 @@ class JncityApi extends ApiAction
         try {
             $ret = Yii::$app->ebook->generateVideoBase64WithEbookStory($ebookStoryId, $userId, $poiId, $filePath);
 //            sleep(3);
-            $ret = false;
+//            $ret = false;
             if ($ret === false) {
                 Yii::error('[JNCITY]生成视频失败，返回结果：' . json_encode($ret, JSON_UNESCAPED_UNICODE));
                 throw new \Exception('AI错误', ErrorCode::EBOOK_GEN_VIDEO_FAILED);
